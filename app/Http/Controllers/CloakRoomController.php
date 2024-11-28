ab19698c6ba636a7670c8117ee0b99057f879a05
dipanshu <dipanshuchauhan23@gmail.com>
2024-11-28 18:47:55 +0530

    cloackroom controller changes

--- app/Http/Controllers/CloakRoomController.php
+++ app/Http/Controllers/CloakRoomController.php
@@ -297,7 +297,7 @@ class CloakRoomController extends Controller {
 	    	$l_entry = CloakRoom::where('unique_id', $productName)->first();
     	}
 
-    	dd($productName);
+    	dd($l_entry);
 
     	
     	$entry_id = $l_entry->id;

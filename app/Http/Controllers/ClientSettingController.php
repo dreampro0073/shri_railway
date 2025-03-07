3640be49b2f118b36bb120a46a1df7048e066cfb
dipanshu <dipanshuchauhan23@gmail.com>
2025-03-07 20:43:03 +0530

    total UPI collection

--- app/Http/Controllers/ClientSettingController.php
+++ app/Http/Controllers/ClientSettingController.php
@@ -73,9 +73,9 @@ class ClientSettingController extends Controller {
         foreach ($clients as $key => $client) {
             $service_ids = DB::table('client_services')->where('client_id',$client->id)->where('services_id','!=',3)->pluck('services_id')->toArray();
 
-            $shifts = Shift::getStatus($request, $client->id,  $service_ids);
+            $c_shift = Shift::getStatus($request, $client->id,  $service_ids);
 
-            // foreach ($shifts as $key => $shift) {
+            // foreach ($c_shift as $key => $shift) {
                 
             //     $client->total_collection = $shift->total_collection;
             //     $client->total_shift_cash = $shift->total_shift_cash;
@@ -83,13 +83,11 @@ class ClientSettingController extends Controller {
 
             // }
 
-            if($shifts){
-                $client->total_collection = $shift->total_collection;
-                $client->total_shift_cash = $shift->total_shift_cash;
-                $client->total_shift_upi = $shift->total_shift_upi;
+            if($c_shift){
+                $client->total_collection = $c_shift->total_collection;
+                $client->total_shift_cash = $c_shift->total_shift_cash;
+                $client->total_shift_upi = $c_shift->total_shift_upi;
             }
-
-            $client->shifts = $shifts;
         }
 
         $data['success'] = true;

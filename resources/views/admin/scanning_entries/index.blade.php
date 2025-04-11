798b6c8398593ee469a4084e9d059bab3dfecd17
dipanshu <dipanshuchauhan23@gmail.com>
2025-04-11 10:05:51 +0530

    local changes

--- resources/views/admin/scanning_entries/index.blade.php
+++ resources/views/admin/scanning_entries/index.blade.php
@@ -47,11 +47,11 @@
                             <td>@{{ item.no_of_item }}</td>
                             <td>@{{ item.paid_amount }}</td>
                             <td>@{{ item.show_pay_type }}
-                                <!-- @if(in_array(Auth::user()->client_id,$client_ids)) -->
+                                
                                 <span ng-if="item.added_by == {{ Auth::id() }}">
                                     <a onclick="return confirm('Are you sure?')" ng-if="item.checkout_status != 1" href="{{url('/admin/scanning/change-pay-type')}}/@{{item.id}}" style="font-size: 15px;"><i class="fa fa-edit"> </i></a>
                                 </span>
-                                <!-- @endif -->
+                               
                             </td>
                             <td>@{{ item.incoming_type }}</td>
                             <td>

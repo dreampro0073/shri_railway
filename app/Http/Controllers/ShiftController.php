97609b647b3218164f5f9615c20b139601c84c7b
dipanshu <dipanshuchauhan23@gmail.com>
2025-05-20 12:46:09 +0530

    local changes

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -37,8 +37,8 @@ class ShiftController extends Controller {
 		}
 
 		$service_ids = Entry::getServiceIds($client_id);
-		// $data = $this->getStatus($request->all(), $client_id, $service_ids);
-		$data = Shift::getStatus($request->all(), $client_id, $service_ids);
+		$data = $this->getStatus($request->all(), $client_id, $service_ids);
+		// $data = Shift::getStatus($request->all(), $client_id, $service_ids);
 		$data['success'] = true;
 		$data['users'] = DB::table('users')->select('id','name')->where('priv','!=',4)->where("client_id", $client_id)->get();
 		$data['service_ids'] = $service_ids;
@@ -49,8 +49,8 @@ class ShiftController extends Controller {
 	public function print(Request $request, $type = 1){
 		$client_id = isset($request->client_id) ? $request->client_id : Auth::user()->client_id;
 		$service_ids = Entry::getServiceIds($client_id);
-		// $data = $this->getStatus($request->all(), $client_id, $service_ids);
-		$data = Shift::getStatus($request->all(), $client_id, $service_ids);
+		$data = $this->getStatus($request->all(), $client_id, $service_ids);
+		// $data = Shift::getStatus($request->all(), $client_id, $service_ids);
 		$current_shift = Entry::checkShift();
 		
         return view('admin.print_shift',[
@@ -72,84 +72,84 @@ class ShiftController extends Controller {
         ]);
 	}
 
-	// public function getStatus($request, $client_id, $service_ids){ 
-	// 	$input_date = isset($request['input_date']) ? $request['input_date'] : date("Y-m-d");
-	// 	if(Auth::user()->priv != 2){
-    //         $user_id = Auth::id();
-    //     } else{
-	// 		$user_id = isset($request['user_id']) ? $request['user_id'] : 0;
-	// 	}
+	public function getStatus($request, $client_id, $service_ids){ 
+		$input_date = isset($request['input_date']) ? $request['input_date'] : date("Y-m-d");
+		if(Auth::user()->priv != 2){
+            $user_id = Auth::id();
+        } else{
+			$user_id = isset($request['user_id']) ? $request['user_id'] : 0;
+		}
 		 
-	// 	$current_shift = Entry::checkShift();
+		$current_shift = Entry::checkShift();
 
-	// 	$data['total_shift_upi'] = 0;
-	// 	$data['total_shift_cash'] = 0;
-	// 	$data['total_collection'] = 0;
-	// 	$data['last_hour_upi_total'] = 0;
-	// 	$data['last_hour_cash_total'] = 0;
-	// 	$data['last_hour_total'] = 0;
+		$data['total_shift_upi'] = 0;
+		$data['total_shift_cash'] = 0;
+		$data['total_collection'] = 0;
+		$data['last_hour_upi_total'] = 0;
+		$data['last_hour_cash_total'] = 0;
+		$data['last_hour_total'] = 0;
 
-	// 	if(in_array(1, $service_ids)){
-	// 		$sitting_data = Sitting::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['sitting_data'] = $sitting_data;
-	// 		$data = $this->calculateAmount($sitting_data, $data);
-	// 		if($user_id && Auth::user()->priv == 2){
-	// 			$data['chage_pay_type_data'] = Sitting::getChangePayTypeLog($input_date, $user_id);
-	// 		}
-	// 	}
+		if(in_array(1, $service_ids)){
+			$sitting_data = Sitting::totalShiftData($input_date,$user_id,$client_id);
+			$data['sitting_data'] = $sitting_data;
+			$data = $this->calculateAmount($sitting_data, $data);
+			if($user_id && Auth::user()->priv == 2){
+				$data['chage_pay_type_data'] = Sitting::getChangePayTypeLog($input_date, $user_id);
+			}
+		}
 
-	// 	if(in_array(2, $service_ids)){
-	// 		$cloak_data = CloakRoom::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['cloak_data'] = $cloak_data;
-	// 		$data = $this->calculateAmount($cloak_data, $data);
-	// 	}
+		if(in_array(2, $service_ids)){
+			$cloak_data = CloakRoom::totalShiftData($input_date,$user_id,$client_id);
+			$data['cloak_data'] = $cloak_data;
+			$data = $this->calculateAmount($cloak_data, $data);
+		}
 		
-	// 	if(in_array(3, $service_ids)){
-	// 		$canteen_data = Canteen::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['canteen_data'] = $canteen_data;
-	// 		$data = $this->calculateAmount($canteen_data, $data);
-	// 	}		
+		if(in_array(3, $service_ids)){
+			$canteen_data = Canteen::totalShiftData($input_date,$user_id,$client_id);
+			$data['canteen_data'] = $canteen_data;
+			$data = $this->calculateAmount($canteen_data, $data);
+		}		
 
-	// 	if(in_array(4, $service_ids)){
-	// 		$massage_data = Massage::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['massage_data'] = $massage_data;
-	// 		$data = $this->calculateAmount($massage_data, $data);
-	// 	}		
+		if(in_array(4, $service_ids)){
+			$massage_data = Massage::totalShiftData($input_date,$user_id,$client_id);
+			$data['massage_data'] = $massage_data;
+			$data = $this->calculateAmount($massage_data, $data);
+		}		
 
-	// 	if(in_array(5, $service_ids)){
-	// 		$locker_data = Locker::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['locker_data'] = $locker_data;
-	// 		$data = $this->calculateAmount($locker_data, $data);
-	// 	}
+		if(in_array(5, $service_ids)){
+			$locker_data = Locker::totalShiftData($input_date,$user_id,$client_id);
+			$data['locker_data'] = $locker_data;
+			$data = $this->calculateAmount($locker_data, $data);
+		}
 
-	// 	if(in_array(7, $service_ids)){
-	// 		$recliner_data = Recliner::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['recliner_data'] = $recliner_data;
-	// 		$data = $this->calculateAmount($recliner_data, $data);
-	// 	}		
+		if(in_array(7, $service_ids)){
+			$recliner_data = Recliner::totalShiftData($input_date,$user_id,$client_id);
+			$data['recliner_data'] = $recliner_data;
+			$data = $this->calculateAmount($recliner_data, $data);
+		}		
 
-	// 	if(in_array(8, $service_ids)){
-	// 		$pod_data = Room::totalShiftData(1,$input_date,$user_id,$client_id);
-	// 		$data['pod_data'] = $pod_data;
-	// 		$data = $this->calculateAmount($pod_data, $data);
+		if(in_array(8, $service_ids)){
+			$pod_data = Room::totalShiftData(1,$input_date,$user_id,$client_id);
+			$data['pod_data'] = $pod_data;
+			$data = $this->calculateAmount($pod_data, $data);
 
-	// 		$singal_cabin_data = Room::totalShiftData(2,$input_date,$user_id,$client_id);
-	// 		$data['singal_cabin_data'] = $singal_cabin_data;
-	// 		$data = $this->calculateAmount($singal_cabin_data, $data);	
+			$singal_cabin_data = Room::totalShiftData(2,$input_date,$user_id,$client_id);
+			$data['singal_cabin_data'] = $singal_cabin_data;
+			$data = $this->calculateAmount($singal_cabin_data, $data);	
 
-	// 		$double_bed_data = Room::totalShiftData(3,$input_date,$user_id,$client_id);
-	// 		$data['double_bed_data'] = $double_bed_data;
-	// 		$data = $this->calculateAmount($double_bed_data, $data);
-	// 	}
+			$double_bed_data = Room::totalShiftData(3,$input_date,$user_id,$client_id);
+			$data['double_bed_data'] = $double_bed_data;
+			$data = $this->calculateAmount($double_bed_data, $data);
+		}
 
-	// 	if(in_array(9, $service_ids)){
-	// 		$scanning_data = ScanningEntry::totalShiftData($input_date,$user_id,$client_id);
-	// 		$data['scanning_data'] = $scanning_data;
-	// 		$data = $this->calculateAmount($scanning_data, $data);
-	// 	}
+		if(in_array(9, $service_ids)){
+			$scanning_data = ScanningEntry::totalShiftData($input_date,$user_id,$client_id);
+			$data['scanning_data'] = $scanning_data;
+			$data = $this->calculateAmount($scanning_data, $data);
+		}
 		
-	// 	return $data;
-	// }
+		return $data;
+	}
 
 	public function calculateAmount($total_data, $data){
 

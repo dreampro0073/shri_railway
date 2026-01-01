55e6c7a93f38983ec5703241f7796447247cf6c4
dipanshu73 <dipanshuchauhan23@gmail.com>
2026-01-01 17:28:35 +0530

    SEO Changes

--- resources/views/front_end/layout.blade.php
+++ resources/views/front_end/layout.blade.php
@@ -28,9 +28,21 @@
     <link href="{{ url('front-end/css/custom.css?v=1.0.5') }}" rel="stylesheet">
 
     @yield('header_scripts')
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TXNZ7HBK');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>  
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXNZ7HBK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('front_end.header')
 
     @yield('main')

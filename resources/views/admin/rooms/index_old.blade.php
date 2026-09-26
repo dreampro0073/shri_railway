b8b009f18c86ce804ed97e20c45163da52c06741
dipanshu73 <dipanshuchauhan23@gmail.com>
2026-09-26 12:57:19 +0530

    local merge with rooms

---
+++ resources/views/admin/rooms/index_old.blade.php
@@ -0,0 +1,158 @@
+@extends('admin.layout')
+
+@section('main')
+    <div class="main" ng-controller="entryRoomCtrl" ng-init="type = {{$type}};init();"> 
+        @include('admin.rooms.add')
+        <div class="card shadow mb-4 p-4">    
+            <div class="filters" style="margin:24px 0;">
+                <form name="filterForm"  novalidate>
+                    <div class="row" style="font-size: 14px;">
+                        <div class="col-md-7">
+                            <div class="row">
+                               <!--  <div class="col-md-3 form-group">
+                                    <label class="label-control">Booking Id</label>
+                                    <input type="text" class="form-control" ng-model="filter.id" />
+                                </div>  --> 
+                                <div class="col-md-3 form-group">
+                                    <label class="label-control">Bill Number</label>
+                                    <input type="text" class="form-control" ng-model="filter.unique_id" />
+                                </div>                    
+                                <div class="col-md-3 form-group">
+                                    <label class="label-control">Name</label>
+                                    <input type="text" class="form-control" ng-model="filter.name" />
+                                </div>                    
+                                <div class="col-md-3 form-group">
+                                    <label class="label-control">Mobile</label>
+                                    <input type="text" class="form-control" ng-model="filter.mobile_no" />
+                                </div>
+                                <div class="col-md-3 form-group">
+                                    <label class="label-control">PNR</label>
+                                    <input type="text" class="form-control" ng-model="filter.pnr_uid" />
+                                </div>
+                               
+                            </div>
+                        </div>
+                        <div class="col-md-5 text-right align-content-end" style="margin-top: 27px;" class="mb-2">
+                            <button type="button" ng-click="init()" class="btn btn-warning-600  align-items-center gap-6 d-inline-flex" >
+                                <span class="d-flex text-md">
+                                  <i class="ri-search-line"></i>
+                                </span>
+                                Search
+                            </button>
+                            <button type="button" ng-click="filterClear()" class="btn btn-danger-600  align-items-center gap-6 d-inline-flex" >
+                                <span class="d-flex text-md">
+                                  <i class="ri-blur-off-line"></i>
+                                </span>
+                                Clear
+                            </button>
+                            <button type="button" ng-if="type !=4" ng-click="add()" class="btn btn-primary-600  align-items-center gap-6 d-inline-flex" >
+                                <span class="d-flex text-md">
+                                  <i class="ri-add-large-line"></i>
+                                </span>
+                                Add
+                            </button>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+          
+            <div class="dt-layout-row dt-layout-table">
+                <div class="dt-layout-cell">
+                    <table class="table table-bordered table-striped" >
+                        <thead >
+                            <tr>
+                                <th>S.no</th>
+                                <th>Bill no</th>
+                                <th>
+                                    <span ng-if="type == 1">Pod</span>
+                                    <span ng-if="type == 2">Cabin</span>
+                                    <span ng-if="type == 3">Bed</span>
+                                    <span ng-if="type == 4">Online</span>
+                                </th>
+                                <th>Name</th>
+                                <th>Mobile No</th>
+                                <th>Check In/Check Out</th>
+                                <th>PNR</th>
+                                <th>Pay Type/Hr</th>
+                                <th>Paid Amount</th>
+                                <th>Discount Amount</th>
+                                <th>#</th>
+                            </tr>
+                        </thead>
+                        <tbody ng-if="entries.length > 0" >
+                            <tr ng-repeat="item in entries " ng-class="item.check_class">
+                                <td>@{{ $index+1 }}</td>
+                                <td>@{{ item.unique_id }}</td>
+                                <td>
+                                    <span ng-if="item.type == 1" style="font-size:12px;">
+                                        Pods -
+                                    </span>
+                                    <span ng-if="item.type == 2" style="font-size:12px;">
+                                        Single Cabin -
+                                    </span>
+                                    <span ng-if="item.type == 3" style="font-size:12px;">
+                                        Double Bed -
+                                    </span>
+                                    <span>
+                                        @{{ item.show_e_ids }}
+                                    </span>
+                                </td>
+                                <td>@{{ item.name }}</td>
+                                <td>@{{ item.mobile_no }}</td>
+                                <td>@{{ item.check_in }}/@{{ item.checkout_date }}</td>
+                                
+                                <td>@{{ item.pnr_uid }}</td>
+                                
+                                <td>
+                                    <span ng-if="item.pay_type == 1">Cash</span>
+                                    <span ng-if="item.pay_type == 2">UPI</span>
+                                    <span>/@{{item.hours_occ+item.late_hr}}Hr</span>
+                                </td>  
+                                
+                                <td>@{{ item.sh_paid_amount}}</td>
+                                <td>@{{item.discount_amount }}</td>
+                                
+                                <td>
+                                    <a ng-if="item.status == 1" href="javascript:;" ng-click="checkoutLoker(item.id)" class="btn btn-danger-600  btn-sm align-items-center gap-6 d-inline-flex">
+                                        <span>Checkout</span>
+                                    </a>
+
+                                    <a ng-if="item.status == 1" href="javascript:;" ng-click="edit(item.id)" class="btn btn-warning-600  btn-sm align-items-center gap-6 d-inline-flex">
+                                        <span>
+                                            Edit
+                                        </span>
+                                    </a>
+
+
+                                    @if(Auth::user()->priv == 2)
+                                       <a onclick="return confirm('Are you sure?')" href="{{url('/admin/rooms/checkout-without-penalty')}}/@{{item.id}}" ng-if="item.checkout_status != 1 && item.status == 1" ng-class="item.check_class == 't-danger' " class="btn btn-warning-600  btn-sm align-items-center gap-6 d-inline-flex">
+                                           <span>
+                                               Checkout WP
+                                           </span>
+                                       </a>
+                                    @endif
+
+                                    <a ng-click="markCheckin(item.id)" ng-if="item.online_booking == 1 && item.status == 0" href="javascript:;" class="btn btn-primary-600  btn-sm align-items-center gap-6 d-inline-flex">
+                                        <span>Mark CheckIn</span>
+                                    </a>
+
+                                    <a href="{{url('/admin/rooms/print')}}/@{{item.id}}" class="btn btn-primary-600  btn-sm align-items-center gap-6 d-inline-flex" target="_blank">
+                                        <span>Print</span>
+                                    </a>
+
+                                    <a ng-click="deleteBooking(item.id)" ng-if="type == 4" class="btn btn-danger-600  btn-sm align-items-center gap-6 d-inline-flex" href="javascript:;">
+                                        <span>Delete</span>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div ng-if="entries.length == 0" class="alert alert-danger">Data Not Found!</div>
+                </div>
+            </div>     
+        </div>
+    </div>
+@endsection
+    
+    
